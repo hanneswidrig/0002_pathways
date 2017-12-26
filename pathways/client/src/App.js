f11a5567322ed3e8412dfb49d84c4ccffd0792b1
Hannes Widrig <hanneswidrig@gmail.com>
2017-12-26 01:02:03 -0600

    Trying to figure out states

--- pathways/client/src/App.js
+++ pathways/client/src/App.js
@@ -2,24 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {stories: []}
+	constructor() {
+		super();
+		this.state = {
+			stories: []
+		}
+	}
 
-  componentDidMount() {
-    fetch('/stories')
-      .then(res => res.json())
-      .then(stories => this.setState({ stories }));
-  }
+	componentDidMount() {
+		fetch('/stories')
+		.then(results => results.json() )
+		.then(stories => this.setState( {stories}, this.getList ) );
+	}
+
+	getList() {
+		let stories = this.state.stories;
+		console.log(stories[0].id);
+		console.log(stories[0].story_entry);
+	}
 
   render() {
     return (
       <div className="App">
-        <h1>Stories</h1>
-        {this.state.stories.map(story =>
-          <div key={story.id}>{story.story_entry}</div>
-        )}
+				<div className="BeforeCenter">{this.state.stories[0].id}</div>
+				<Entry />
       </div>
     );
   }
 }
 
+class Entry extends Component {
+	render() {
+		return(
+			<div className="CenterText">{}</div>
+		);
+	}
+}
+
 export default App;
+
+/* const stories = [
+	{id: 0, story_entry: 'Welcome to the Jungle'},
+	{id: 1, story_entry: 'What is your name?'},
+	{id: 2, story_entry: 'You enter the forest'}
+]; */
+
+// {this.state.stories.map( (stories) =>
+// <div className="CenterText" key={stories.id}>{stories.story_entry}</div>)}
--- pathways/routes/stories.js
+++ pathways/routes/stories.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET stories listing. */
 router.get('/', function(req, res, next) {
-	res.json([{id: 1, story_entry: "..."}]);
+	res.json([
+		{id: 0, story_entry: 'Welcome to the Jungle'},
+		{id: 1, story_entry: 'What is your name?'},
+		{id: 2, story_entry: 'You enter the forest'}
+	]);
 });
 
 module.exports = router;
