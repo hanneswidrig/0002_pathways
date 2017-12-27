e18609530830a82e46ae5946aea03cd18911adc8
Hannes Widrig <hanneswidrig@gmail.com>
2017-12-26 21:05:31 -0600

    Working navigation with arrow keys

--- pathways/client/src/App.js
+++ pathways/client/src/App.js
@@ -1,51 +1,93 @@
 import React, { Component } from 'react';
+import ArrowKeysReact from 'arrow-keys-react';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			stories: []
+			stories: [{id: '', story_entry: ''}],
+			currentLoc: 0
+		}
+		ArrowKeysReact.config({
+			left:  () => {
+				this.prevStory();
+			},
+			right: () => {
+				this.nextStory();
+			},
+			up:    () => {
+				this.prevStory();
+			},
+			down:  () => {
+				this.nextStory();
+			}
+		});
+	}
+
+	prevStory() {
+		this.setState( (prevState, props) => {
+			if (prevState.currentLoc > 0) {
+				return {currentLoc: prevState.currentLoc - 1};
+			}
+			else if (prevState.currentLoc === 0) {
+				return {currentLoc: prevState.currentLoc};
+			}
+		});
+	}
+	nextStory() {
+		this.setState( (prevState, props) => {
+			if (prevState.currentLoc === (prevState.stories.length - 1) ) {
+				return {currentLoc: prevState.currentLoc};
+			}
+			else {
+				return {currentLoc: prevState.currentLoc + 1};
+			}
+		});
+	}
+	isFirstStory() {
+		if(this.state.currentLoc === 0) {
+			return true;
+		}
+		else {
+			return false;
 		}
 	}
 
 	componentDidMount() {
 		fetch('/stories')
 		.then(results => results.json() )
-		.then(stories => this.setState( {stories}, this.getList ) );
-	}
-
-	getList() {
-		let stories = this.state.stories;
-		console.log(stories[0].id);
-		console.log(stories[0].story_entry);
+		.then(stories => this.setState({stories}) );
 	}
 
   render() {
     return (
-      <div className="App">
-				<div className="BeforeCenter">{this.state.stories[0].id}</div>
-				<Entry />
+      <div {...ArrowKeysReact.events} tabIndex="1" className="App">
+				<div className="storyId">{this.state.currentLoc}</div>
+				{
+					this.isFirstStory() ? null : 
+					<PreviousEntry story_entry={this.state.stories[this.state.currentLoc - 1].story_entry}/>
+				}
+				<Entry story_entry={this.state.stories[this.state.currentLoc].story_entry}/>
       </div>
     );
   }
 }
 
+class PreviousEntry extends Component {
+	render() {
+		return(
+			<div className="BeforeCenter">{this.props.story_entry}</div>
+		);
+	}
+}
+
 class Entry extends Component {
 	render() {
 		return(
-			<div className="CenterText">{}</div>
+			<div className="CenterText">{this.props.story_entry}</div>
 		);
 	}
 }
 
 export default App;
-
-/* const stories = [
-	{id: 0, story_entry: 'Welcome to the Jungle'},
-	{id: 1, story_entry: 'What is your name?'},
-	{id: 2, story_entry: 'You enter the forest'}
-]; */
-
-// {this.state.stories.map( (stories) =>
-// <div className="CenterText" key={stories.id}>{stories.story_entry}</div>)}
--- pathways/routes/stories.js
+++ pathways/routes/stories.js
@@ -6,7 +6,8 @@ router.get('/', function(req, res, next) {
 	res.json([
 		{id: 0, story_entry: 'Welcome to the Jungle'},
 		{id: 1, story_entry: 'What is your name?'},
-		{id: 2, story_entry: 'You enter the forest'}
+		{id: 2, story_entry: 'You enter the forest'},
+		{id: 3, story_entry: 'Before you there is a fork in the path'}
 	]);
 });
 
