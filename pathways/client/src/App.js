d0c61ebbb8bf1620ac58f1946f77275df137d5de
hanneswidrig <hanneswidrig@gmail.com>
2018-01-26 19:13:32 -0500

    Stylization changes

--- pathways/client/src/App.js
+++ pathways/client/src/App.js
@@ -6,76 +6,78 @@ import PreviousEntry from './components/PreviousEntry';
 import Timeline from './components/Timeline';
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			stories: [{id: '', story_entry: ''}],
-			currentLoc: 0
-		}
-		ArrowKeysReact.config({
-			left:  () => {
-				this.prevStory();
-			},
-			right: () => {
-				this.nextStory();
-			},
-			up:    () => {
-				this.prevStory();
-			},
-			down:  () => {
-				this.nextStory();
-			}
-		});
-	}
+  constructor() {
+    super();
+    this.state = {
+      stories: [{ id: '', story_entry: '' }],
+      currentLoc: 0
+    };
+    ArrowKeysReact.config({
+      left: () => {
+        this.prevStory();
+      },
+      right: () => {
+        this.nextStory();
+      },
+      up: () => {
+        this.prevStory();
+      },
+      down: () => {
+        this.nextStory();
+      }
+    });
+  }
 
-	prevStory() {
-		this.setState( (prevState, props) => {
-			if (prevState.currentLoc > 0) {
-				return {currentLoc: prevState.currentLoc - 1};
-			}
-			else if (prevState.currentLoc === 0) {
-				return {currentLoc: prevState.currentLoc};
-			}
-		});
-	}
-	nextStory() {
-		this.setState( (prevState, props) => {
-			if (prevState.currentLoc === (prevState.stories.length - 1) ) {
-				return {currentLoc: prevState.currentLoc};
-			}
-			else {
-				return {currentLoc: prevState.currentLoc + 1};
-			}
-		});
-	}
-	isFirstStory() {
-		if(this.state.currentLoc === 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+  prevStory() {
+    this.setState((prevState, props) => {
+      if (prevState.currentLoc > 0) {
+        return { currentLoc: prevState.currentLoc - 1 };
+      } else if (prevState.currentLoc === 0) {
+        return { currentLoc: prevState.currentLoc };
+      }
+    });
+  }
+  nextStory() {
+    this.setState((prevState, props) => {
+      if (prevState.currentLoc === prevState.stories.length - 1) {
+        return { currentLoc: prevState.currentLoc };
+      } else {
+        return { currentLoc: prevState.currentLoc + 1 };
+      }
+    });
+  }
+  isFirstStory() {
+    if (this.state.currentLoc === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	componentDidMount() {
-		fetch('/stories')
-		.then(results => results.json() )
-		.then(stories => this.setState({stories}) );
-	}
+  componentDidMount() {
+    fetch('/stories')
+      .then(results => results.json())
+      .then(stories => this.setState({ stories }));
+  }
 
   render() {
     return (
       <div {...ArrowKeysReact.events} tabIndex="1" className="App">
-				<div className="Entries">
-				<div className="StoryId">{this.state.currentLoc}</div>
-				{
-					this.isFirstStory() ? null : 
-					<PreviousEntry story_entry={this.state.stories[this.state.currentLoc - 1].story_entry}/>
-				}
-				<Entry story_entry={this.state.stories[this.state.currentLoc].story_entry}/>
-				</div>
-				<Timeline />
-			</div>
+        <div className="Entries">
+          <div className="StoryId">{this.state.currentLoc}</div>
+          {this.isFirstStory() ? null : (
+            <PreviousEntry
+              story_entry={
+                this.state.stories[this.state.currentLoc - 1].story_entry
+              }
+            />
+          )}
+          <Entry
+            story_entry={this.state.stories[this.state.currentLoc].story_entry}
+          />
+        </div>
+        <Timeline />
+      </div>
     );
   }
 }
--- pathways/client/src/components/Timeline.js
+++ pathways/client/src/components/Timeline.js
@@ -4,29 +4,31 @@ import TreeView from 'react-treeview';
 const dataSource = [
   ['Welcome to the Jungle', 'What is your name?'],
   ['You enter the forest', 'Before you there is a fork in the path'],
-  ['A scary monster jumps out at you', 'You run for your life...'],
+  ['A scary monster jumps out at you', 'You run for your life...']
 ];
 
 class Timeline extends Component {
-	render() {
-		return(
-			<div className="Timeline">
-				{
-					dataSource.map( (node, i) => {
-						return (
-							<TreeView
-								key={i}
-								defaultCollapsed={false}
-								nodeLabel={'Chapter ' + i}>
-								{node.map(entry => 
-									<div className="info" key={entry+'-'+i}>{entry}</div>)}
-							</TreeView>
-						);
-					})
-				}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="Timeline">
+        {dataSource.map((node, i) => {
+          return (
+            <TreeView
+              key={i}
+              defaultCollapsed={false}
+              nodeLabel={'Chapter ' + i}
+            >
+              {node.map(entry => (
+                <div className="info" key={entry + '-' + i}>
+                  {entry}
+                </div>
+              ))}
+            </TreeView>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Timeline;
